0d36d646c7c2526b84c7a58038bb8608ee9b863c
Jonathan Morris <jonathan_wellington@icloud.com>
2024-07-19 17:30:12 -0700

    Added movement Cod3e

--- slug-mobile.py
+++ slug-mobile.py
@@ -5,7 +5,7 @@ from adafruit_servokit import ServoKit
 
 class SlugMobile:
 
-    def __init__(self, max_steering=80, max_throttle=20):
+    def __init__(self, max_steering=80, max_throttle=20, i2c_address=0x60, channels=16):
         """
         max_steering: max percentage of steering angle
         max_throttle: max percentage of thrust
@@ -16,5 +16,22 @@ class SlugMobile:
         self.steering_angle = 0
         self.throttle = 0
 
-        self.servo_kit = ServoKit(address=0x60, channels=16)
+        self.servo_kit = ServoKit(address=i2c_address, channels=channels)
+        self.DrivingServo = self.servo_kit.servo[15]
+        self.SteeringServo = self.servo_kit.servo[0]
 
+        self.DrivingServo.angle = 90
+        self.SteeringServo.angle = 90
+
+        sleep(5)
+
+    def num_to_range(num, inMin, inMax, outMin, outMax):
+        return round(
+            outMin + (float(num - inMin) / float(inMax - inMin) * (outMax - outMin)), 3
+        )
+
+    def set_steering_angle(self, angle):
+        self.SteeringServo.angle = angle
+
+    def set_throttle(self, throttle):
+        self.DrivingServo.angle = throttle
