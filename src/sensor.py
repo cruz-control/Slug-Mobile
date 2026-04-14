5328c8ec0b368f2d7c2e7bda33c549faa07d6a36
Njeri Gachoka <njerigachoka@gmail.com>
2026-04-13 18:08:12 -0700

    self, not slef

--- src/sensor.py
+++ src/sensor.py
@@ -1,19 +1,22 @@
 from pubsub import set_topic
 
+
 class Sensor:
     """
     Base class for sensors
     """
-    def __init__(slef):
+
+    def __init__(self):
         pass
+
     def update(self):
         """
         Updates the sensor if needed. This must be implemented, and it should call set_topic().
         """
         raise NotImplementedError("update() needs to be implemented")
+
     def set_topic(self, topic: str, value: any) -> None:
         """
         Calling set_topic on a sensor is the same as calling it on pubsub.
         """
         set_topic(topic, value)
-
