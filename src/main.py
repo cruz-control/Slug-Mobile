bbf7c7ae2e611d819cdb200ad94c0ddfa145d155
Kyle-Eldridge <keldridge07@gmail.com>
2026-05-11 18:33:07 -0700

    Fix bug

--- src/main.py
+++ src/main.py
@@ -19,6 +19,7 @@ initializing = len(nodes)
 
 def run_thread(node_class):
     global stop, loop_time, initializing
+    initialized = False
     start = time.time()
     try:
         # Detect if it is a class or object
@@ -28,6 +29,7 @@ def run_thread(node_class):
             node = node_class
         node.start()
         initializing -= 1
+        initialized = True
         while initializing > 0 and not stop:
             time.sleep(loop_time)
         while not stop:
@@ -37,9 +39,12 @@ def run_thread(node_class):
                 time.sleep(loop_time - (end-start))
             start = end
     except Exception as e:
+        if not initialized:
+            initializing -= 1
         print(f'Error in {node_class.__name__}: {e}')
     try:
-        node.stop()
+        if node is not None:
+            node.stop()
     except Exception as e:
         print(f'Error stopping {node_class.__name__}: {e}')
 
